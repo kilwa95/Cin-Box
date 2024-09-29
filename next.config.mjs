33978e1379ad43001955929fe0d150f7be3a23ce
khaled Abdulhalim <kabdulhalim@reseau.free.fr>
2024-09-29 13:24:49 +0200

    Install Sass and setup initial configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [join(__dirname, 'src/styles')],
+  },
+};
 
 export default nextConfig;
---
+++ src/app/movies/page.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MoviesPage = () => {};
+
+export default MoviesPage;
