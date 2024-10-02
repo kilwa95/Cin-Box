5b760ac221d31823caab3de6ee71dd2f4757c716
khaled Abdulhalim <kabdulhalim@reseau.free.fr>
2024-10-02 10:05:09 +0200

    create MovieSearch & MovieSearchResults

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import MediaCard from '@/components/media-card/MediaCard';
+import Popular from '../components/popular/Popular';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <MediaCard />
+      <Popular />
     </div>
   );
 }
