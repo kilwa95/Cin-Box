31d9ae03d983bc719919044d52569cf56c1bd9da
khaled Abdulhalim <kabdulhalim@reseau.free.fr>
2024-10-02 10:33:19 +0200

    Displaying media genres

---
+++ src/app/movies/genres/[id]/page.js
@@ -0,0 +1,9 @@
+const GenresIdPage = ({ params }) => {
+  return (
+    <div>
+      <h1>Genres page with id : {params.id}</h1>
+    </div>
+  );
+};
+
+export default GenresIdPage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Popular from '../components/popular/Popular';
+import Genres from '../components/Genres/Genres';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div className={styles.main}>
       <Popular />
+      <Genres />
     </div>
   );
 }
