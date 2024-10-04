0a7d34120e2069ea9e249938ed13186a86cb3827
khaled Abdulhalim <kabdulhalim@reseau.free.fr>
2024-10-04 09:52:24 +0200

    Movie Images and Title

--- src/app/movies/[id]/page.js
+++ src/app/movies/[id]/page.js
@@ -1,7 +1,21 @@
-const MovieIdPage = ({ params }) => {
+import { getMovieByPath } from '@/utils/movieClient';
+import { notFound } from 'next/navigation';
+import React from 'react';
+import MovieDetails from '@/components/MovieDetails/MovieDetails';
+
+export const dynamic = 'force-static';
+export const revalidate = 3600;
+
+const MovieIdPage = async ({ params }) => {
+  const movie = await getMovieByPath(`/movie/${params.id}`);
+
+  if (!movie.original_title) {
+    return notFound();
+  }
+
   return (
     <div>
-      <h1>Movie page with id : {params.id}</h1>
+      <MovieDetails movie={movie} />
     </div>
   );
 };
