361bf5d6bd800bd47a21f81d142127b2f9f70c44
khaled Abdulhalim <kabdulhalim@reseau.free.fr>
2024-10-07 16:00:30 +0200

    Adding and displaying popular media

--- src/app/[local]/user/profile/page.js
+++ src/app/[local]/user/profile/page.js
@@ -1,11 +1,36 @@
-import React from 'react';
 import LogoutButton from '@/components/logout-button/LogoutButton';
+import styles from './page.module.scss';
+import { getServerSession } from 'next-auth';
+import prisma from '@/utils/prisma';
+import { getHydratedMovies } from '@/utils/movieClient';
+import MediaCard from '@/components/media-card/MediaCard';
+import { getDictionary } from '@/utils/dictionaries';
 
-const ProfilePage = () => {
+const ProfilePage = async ({ params: { local } }) => {
+  const { user: userSession } = await getServerSession();
+  const i18n = await getDictionary(local);
+
+  const { movieLikes } = await prisma.user.findFirst({
+    where: { email: userSession.email },
+    include: {
+      movieLikes: true,
+    },
+  });
+
+  const movies = await getHydratedMovies(
+    movieLikes.map((movie) => movie.movieId)
+  );
   return (
-    <div>
-      <h1>Vous êtes connecté</h1>
-      <LogoutButton />
+    <div className={styles.profile}>
+      <div className={styles.head}>
+        <h1>{i18n.favoriteMovies.title}</h1>
+        <LogoutButton />
+      </div>
+      <div className={styles.list}>
+        {movies.map((movie) => (
+          <MediaCard media={movie} locale={local} key={movie.id} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/media-card/like/Like.js
@@ -0,0 +1,26 @@
+'use client';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { signIn, useSession } from 'next-auth/react';
+
+const Like = ({ mediaId }) => {
+  const { data: session } = useSession();
+
+  const handleLikeClicked = (e) => {
+    e.preventDefault();
+
+    if (!session) {
+      signIn();
+    }
+    fetch(`/api/like/${mediaId}`, {
+      method: 'POST',
+    });
+  };
+  return (
+    <div onClick={handleLikeClicked}>
+      <FontAwesomeIcon icon={faHeart} />
+    </div>
+  );
+};
+
+export default Like;
